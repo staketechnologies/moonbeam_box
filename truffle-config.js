b1cc390bc9b00352c53b9d9b9e7c63f23dc270c4
hskang9 <hskang9@gmail.com>
2021-01-09 22:52:03 +0900

    update provider to HDWalletProvider

--- truffle-config.js
+++ truffle-config.js
@@ -1,43 +1,19 @@
-const PrivateKeyProvider = require("./private-provider");
+const HDWalletProvider = require("truffle-hdwallet-provider")
 // Standalone Development Node Private Key
 const privateKeyDev =
   "60ed0dd24087f00faea4e2b556c74ebfa2f0e705f8169733b01530ce4c619883";
-// Moonbase Alpha Private Key --> Please change this to your own Private Key with funds
-const privateKeyMoonbase =
-  "d2d0ac4e9a8d6bff910a529477f7ea76c457e7f9e1e7b04d7d85d9cd7c63c1e6";
 
 module.exports = {
   networks: {
-    dev: {
+    dusty: {
       provider: () => {
-        if (!privateKeyDev.trim()) {
-          throw new Error(
-            "Please enter a private key with funds, you can use the default one"
-          );
-        }
-        return new PrivateKeyProvider(privateKeyDev, "http://localhost:9933/", 80);
+        return new HDWalletProvider([privateKeyDev], 'http://127.0.0.1:9933')
       },
-      network_id: 80,
-    },
-    moonbase: {
-      provider: () => {
-        if (!privateKeyMoonbase.trim()) {
-          throw new Error(
-            "Please enter a private key with funds to send transactions to TestNet"
-          );
-        }
-        if (privateKeyDev == privateKeyMoonbase) {
-          throw new Error(
-            "Please change the private key used for Moonbase to your own with funds"
-          );
-        }
-        return new PrivateKeyProvider(
-          privateKeyMoonbase,
-          "https://rpc.testnet.moonbeam.network",
-          1287
-        );
-      },
-      network_id: 1287,
+      network_id: "*",       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
   },
 
